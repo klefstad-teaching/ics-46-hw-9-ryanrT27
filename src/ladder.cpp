ed9f2c637a927b489c34d203c4346f272975552e
ryanrT27 <tapiarr@uci.edu>
2025-03-17 05:43:34 +0000

    didnt pass generate or print, requires newline once all generated perchance ?

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -87,6 +87,7 @@ void print_word_ladder(const vector<string>& ladder){
     if (ladder.empty()) return;
     for (size_t i = 0; i < ladder.size(); ++i)
         cout << ladder[i] << " ";
+    cout << endl;
 }
 
 #define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
