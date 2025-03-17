9bacd846a610524478639911b6c39d60b67485cb
ryanrT27 <tapiarr@uci.edu>
2025-03-17 05:46:21 +0000

    boi if u dont stop with these printing errors so picky

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -85,6 +85,7 @@ void load_words(set<string> & word_list, const string& file_name){
 }
 void print_word_ladder(const vector<string>& ladder){
     if (ladder.empty()) return;
+    cout << "Word ladder found: ";
     for (size_t i = 0; i < ladder.size(); ++i)
         cout << ladder[i] << " ";
     cout << endl;
